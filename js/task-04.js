49ba430a9382be304397f1e79cedbd6d065b0fab
Maryna-Bazylevska <marynabazylevska@gmail.com>
2021-09-20 16:53:25 +0200

    add corection on task-04

--- js/task-04.js
+++ js/task-04.js
@@ -1,12 +1,13 @@
 const counterRef = document.querySelector("#counter");
 const decrement = document.querySelector('button[data-action="decrement"]');
 const increment = document.querySelector('button[data-action="increment"]');
+let counterValue = document.querySelector("#value");
 let valueRef = Number(document.querySelector("#value").textContent);
 decrement.addEventListener("click", () => {
   valueRef -= 1;
-  console.log(valueRef);
+  counterValue.innerText = valueRef;
 });
 increment.addEventListener("click", () => {
   valueRef += 1;
-  console.log(valueRef);
+  counterValue.innerText = valueRef;
 });
